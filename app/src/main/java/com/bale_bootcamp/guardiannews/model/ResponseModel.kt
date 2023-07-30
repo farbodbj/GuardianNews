4d6e23c13a6302d24e3bb50714bad84eff9b59d4
Farbod Bijary <110523279+farbodbj@users.noreply.github.com>
2023-07-30 12:42:04 +0330

    first Impl - ResponseModel

--- app/src/main/java/com/bale_bootcamp/guardiannews/model/ResponseModel.kt
+++ app/src/main/java/com/bale_bootcamp/guardiannews/model/ResponseModel.kt
@@ -1,3 +1,9 @@
 package com.bale_bootcamp.guardiannews.model
 
-data class ResponseModel()
+data class ResponseModel(
+    val status: String,
+    val pageSize: Int,
+    val total: Int,
+    val currentPage: Int,
+    val pages: Int,
+    val results: List<News>)
