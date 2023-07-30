e555d7490ab46acae64916798b28c34eb89a5217
Farbod Bijary <110523279+farbodbj@users.noreply.github.com>
2023-07-30 12:37:58 +0330

    first Impl - NewsApiService

--- app/src/main/java/com/bale_bootcamp/guardiannews/network/NewsApiService.kt
+++ app/src/main/java/com/bale_bootcamp/guardiannews/network/NewsApiService.kt
@@ -1,9 +1,53 @@
 package com.bale_bootcamp.guardiannews.network
 
+import com.bale_bootcamp.guardiannews.utility.RetrofitFactory
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+import java.util.Date
+
 object Api {
     const val BASE_URL = "https://content.guardianapis.com/"
+    const val API_KEY = "YOUR_OWN_API_KEY"
 }
 
 
-class NewsApiService {
+interface NewsApiService {
+    enum class Category(private val categoryName: String) {
+        HOME("search"),
+        WORLD("world"),
+        SCIENCE("science"),
+        ENVIRONMENT("environment"),
+        SPORT("sport");
+        override fun toString(): String = this.categoryName
+    }
+
+    @GET("{category}?api-key=${Api.API_KEY}")
+    fun getLatestFromCategory(@Path("category") category: Category,
+                              @Query("from-date") fromDate: Date,
+                              @Query("to-date") toDate: Date,
+                              @Query("page") page: Int,
+                              @Query("page-size") pageSize: Int = 10)
+}
+
+
+object NewsApi {
+    private val okhttpClient: OkHttpClient = OkHttpClient.Builder()
+        .addInterceptor(HttpLoggingInterceptor().apply { level = HttpLoggingInterceptor.Level.BODY })
+        .build()
+
+    private val converterFactory: MoshiConverterFactory = MoshiConverterFactory.create()
+
+    val retrofit: Retrofit = RetrofitFactory.createRetrofitInstance(
+        baseUrl = Api.BASE_URL,
+        client = okhttpClient,
+        converterFactory = converterFactory)
+
+    val retrofitApiService: NewsApiService by lazy {
+        retrofit.create(NewsApiService::class.java)
+    }
 }
