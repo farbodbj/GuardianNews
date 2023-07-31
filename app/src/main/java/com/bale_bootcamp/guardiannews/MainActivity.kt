c495703f2fbb55fdafbc1f60bf3588df83783a2e
Farbod Bijary <110523279+farbodbj@users.noreply.github.com>
2023-07-31 14:20:54 +0330

    view pager set up in the main activity

--- app/src/main/java/com/bale_bootcamp/guardiannews/MainActivity.kt
+++ app/src/main/java/com/bale_bootcamp/guardiannews/MainActivity.kt
@@ -2,10 +2,51 @@ package com.bale_bootcamp.guardiannews
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bale_bootcamp.guardiannews.adapter.NewsPagerAdapter
+import com.bale_bootcamp.guardiannews.databinding.ActivityMainBinding
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
+    lateinit var _binding: ActivityMainBinding
+    val binding get() = _binding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setUiComponents()
+    }
+
+    private fun setUiComponents() {
+        setViewPager()
+        setTabLayout()
+    }
+
+    private fun setViewPager() {
+        val bundle = Bundle()
+        bundle.putString("category", "world")
+
+        binding.viewPager.adapter = NewsPagerAdapter(this@MainActivity)
+            .addFragment(NewsFragment(), listOf("category" to "home"))
+            .addFragment(NewsFragment(), listOf("category" to "world"))
+            .addFragment(NewsFragment(), listOf("category" to "science"))
+            .addFragment(NewsFragment(), listOf("category" to "environment"))
+            .addFragment(NewsFragment(), listOf("category" to "sport"))
+        }
+
+    private fun setTabLayout() {
+        TabLayoutMediator(binding.tabLayout, binding.viewPager) {
+            tab, position ->
+            when(position) {
+                0 -> tab.text = "Home"
+                1 -> tab.text = "World"
+                2 -> tab.text = "Science"
+                3 -> tab.text = "Environment"
+                4 -> tab.text = "Sport"
+            }
+        }.attach()
     }
 }
--- app/src/main/java/com/bale_bootcamp/guardiannews/adapter/NewsPagerAdapter.kt
+++ app/src/main/java/com/bale_bootcamp/guardiannews/adapter/NewsPagerAdapter.kt
@@ -2,21 +2,21 @@ package com.bale_bootcamp.guardiannews.adapter
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentPagerAdapter
-import com.bale_bootcamp.guardiannews.NewsFragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.bale_bootcamp.guardiannews.utility.Utils
 
 
-class NewsPagerAdapter(private val fragmentManager: FragmentManager, private val pages: List<String>): FragmentPagerAdapter(fragmentManager, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
-    override fun getCount(): Int = pages.size
-
-
-    override fun getItem(position: Int): Fragment {
-        val category: Bundle = Bundle()
-        category.putString("category", pages[position])
-
-        val fragment = NewsFragment()
-        fragment.arguments = category
-        return fragment
+class NewsPagerAdapter(fragmentActivity: FragmentActivity): FragmentStateAdapter(fragmentActivity) {
+    private val fragments: MutableList<Fragment> = mutableListOf()
+    fun addFragment(toAdd: Fragment, pariList: List<Pair<String, String>>): NewsPagerAdapter {
+        toAdd.arguments?.putAll(Utils.buildStringBundle(pariList))
+        fragments.add(toAdd)
+        return this
     }
+
+    override fun getItemCount(): Int = fragments.size
+
+    override fun createFragment(position: Int): Fragment = fragments[position]
+
 }
