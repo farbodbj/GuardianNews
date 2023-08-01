6e9884d344b944ea1c2dbb690743cbf90478e78e
Farbod Bijary <110523279+farbodbj@users.noreply.github.com>
2023-08-01 12:09:38 +0330

    more improvements on issue 2

--- app/src/main/java/com/bale_bootcamp/guardiannews/adapter/NewsPagerAdapter.kt
+++ app/src/main/java/com/bale_bootcamp/guardiannews/adapter/NewsPagerAdapter.kt
@@ -5,27 +5,22 @@ import android.util.Log
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
-
-import com.bale_bootcamp.guardiannews.NewsFragmentFactory
-import com.bale_bootcamp.guardiannews.network.NewsApiService
-
+import com.bale_bootcamp.guardiannews.NewsFragment
 
 
 class NewsPagerAdapter(fragmentActivity: FragmentActivity,
-                       private val fragmentFactory: (Int) -> Fragment,
                        private val categories: List<String>
 ): FragmentStateAdapter(fragmentActivity) {
     private val TAG = "NewsPagerAdapter"
 
     override fun getItemCount(): Int {
-        //Log.d(TAG, "getItemCount: ${NewsApiService.Category.values().size}")
+        Log.d(TAG, "getItemCount: ${categories.size}")
         return categories.size
     }
 
     override fun createFragment(position: Int): Fragment {
         Log.d(TAG, "createFragment: position: $position")
-        //return NewsFragmentFactory.newInstance(NewsApiService.Category.values()[position])
-        return fragmentFactory(position)
+        return NewsFragment(categories[position])
     }
 
 }
