7cf65e6e798f525c3c1cb32396f12b919997e483
Farbod Bijary <110523279+farbodbj@users.noreply.github.com>
2023-08-01 16:11:58 +0330

    onSwipeRefreshListener implemented

--- app/src/main/java/com/bale_bootcamp/guardiannews/NewsFragment.kt
+++ app/src/main/java/com/bale_bootcamp/guardiannews/NewsFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.size
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.lifecycleScope
 import com.bale_bootcamp.guardiannews.adapter.NewsAdapter
@@ -21,8 +22,10 @@ import java.time.LocalDate
 class NewsFragment(private val category: String) : Fragment() {
     private val TAG = "NewsFragment"
 
-    private lateinit var _binding: FragmentNewsBinding
-    private val binding get() = _binding
+    private var _binding: FragmentNewsBinding? = null
+    private val binding get() = _binding!!
+
+    private var isRefreshing = false
 
     private val viewModel: NewsFragmentViewModel by activityViewModels {
         NewsFragmentViewModel.NewsFragmentViewModelFactory()
@@ -35,6 +38,9 @@ class NewsFragment(private val category: String) : Fragment() {
         Log.d(TAG, "onViewCreated: binding set")
         setNewsList()
         Log.d(TAG, "onViewCreated: news list set")
+        Log.d(TAG, "onViewCreated: swipe refresh set")
+        setSwipeRefresh()
+        Log.d(TAG, "onViewCreated: swipe refresh set")
     }
 
 
@@ -46,23 +52,38 @@ class NewsFragment(private val category: String) : Fragment() {
         return binding.root
     }
 
-
     private fun setNewsList() {
+        isRefreshing = true
         val newsRecyclerViewAdapter = NewsAdapter {
             Log.d(TAG, "onItemClicked: $it")
-            TODO("onItemClicked")
+            //TODO("onItemClicked")
         }
 
         binding.newsRecyclerView.adapter = newsRecyclerViewAdapter
-        viewModel.getNews(NewsApiService.Category.findByStr(category), LocalDate.parse("2021-09-01"), LocalDate.parse("2021-09-02"), 1, 10).observe(viewLifecycleOwner) { responseModel ->
-            Log.i(TAG, "setNewsList: $responseModel")
-            //checking whether responseModel is null or not
-            if (responseModel?.results != null) {
-                newsRecyclerViewAdapter.submitList(responseModel.results)
-                Log.d(TAG, "setNewsList: list submitted")
-            } else{
-                Log.d(TAG, "setNewsList: responseModel or results is null")
-            }
+        viewModel.getNews(NewsApiService.Category.findByStr(category), LocalDate.parse("2021-09-01"), LocalDate.parse("2021-09-02"), 1, 10)
+            .observe(viewLifecycleOwner) { responseModel ->
+                Log.i(TAG, "setNewsList: $responseModel")
+                //checking whether responseModel is null or not
+                if (responseModel?.results != null) {
+                    newsRecyclerViewAdapter.submitList(responseModel.results)
+                    isRefreshing = false
+                    Log.d(TAG, "setNewsList: list submitted")
+                } else{
+                    Log.d(TAG, "setNewsList: responseModel or results is null")
+                }
         }
+    }
+
+    private fun setSwipeRefresh(){
+        binding.refresh.setOnRefreshListener {
+            setNewsList()
+            if(isRefreshing)
+                binding.refresh.isRefreshing = false
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
