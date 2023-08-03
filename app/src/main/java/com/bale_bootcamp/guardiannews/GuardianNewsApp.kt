ef3270638b75c082ec151625bb6020aaf0960ea9
Farbod Bijary <110523279+farbodbj@users.noreply.github.com>
2023-08-03 12:44:37 +0330

    class for getting app context, database references

---
+++ app/src/main/java/com/bale_bootcamp/guardiannews/GuardianNewsApp.kt
@@ -0,0 +1,35 @@
+package com.bale_bootcamp.guardiannews
+
+import android.app.Application
+import android.util.Log
+import androidx.room.RoomDatabase
+import com.bale_bootcamp.guardiannews.localdatasources.database.AppDatabase
+import java.util.concurrent.Executors.newSingleThreadExecutor
+
+class GuardianNewsApp: Application() {
+    val name = "GuardianNewsApp"
+
+    val database by lazy {
+        AppDatabase.getDatabase(
+                this,
+                object : RoomDatabase.QueryCallback {
+                    override fun onQuery(sqlQuery: String, bindArgs: List<Any?>) {
+                        Log.d("Database", "query: $sqlQuery\n args:$bindArgs")
+                    }
+                },
+                newSingleThreadExecutor()
+            )
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        appContext = this
+    }
+
+    companion object {
+        private lateinit var appContext: GuardianNewsApp
+        fun getAppContext(): GuardianNewsApp {
+            return appContext
+        }
+    }
+}
