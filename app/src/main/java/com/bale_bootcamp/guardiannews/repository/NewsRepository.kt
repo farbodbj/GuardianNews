63d860a9ff08f1033d25ee53d4280828a747d59a
Farbod Bijary <110523279+farbodbj@users.noreply.github.com>
2023-08-03 13:37:27 +0330

    adapted repository and fragment to using flow

--- app/src/main/java/com/bale_bootcamp/guardiannews/repository/NewsRepository.kt
+++ app/src/main/java/com/bale_bootcamp/guardiannews/repository/NewsRepository.kt
@@ -1,13 +1,10 @@
 package com.bale_bootcamp.guardiannews.repository
 
-import android.util.Log
-import androidx.lifecycle.MutableLiveData
+
 import com.bale_bootcamp.guardiannews.localdatasources.database.NewsDao
 import com.bale_bootcamp.guardiannews.model.News
-import com.bale_bootcamp.guardiannews.model.ResponseModel
 import com.bale_bootcamp.guardiannews.network.NewsApiService
 import com.bale_bootcamp.guardiannews.onlinedatasources.NewsOnlineDataSource
-import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.flow.Flow
 import java.time.LocalDate
 
@@ -27,15 +24,15 @@ class NewsRepository (
         results.value?.results?.let {
             localDataSource.insert(*it.toTypedArray())
         }
+
     }
 
 
-    suspend fun getNews(category: NewsApiService.Category,
+    fun getNews(category: NewsApiService.Category,
         fromDate: LocalDate,
         toDate: LocalDate,
         page: Int,
         pageSize: Int
-    ): Flow<List<News>> {
-        return localDataSource.select(category)
-    }
+    ): Flow<List<News>> = localDataSource.select(category)
+
 }
--- app/src/main/java/com/bale_bootcamp/guardiannews/viewmodel/NewsFragmentViewModel.kt
+++ app/src/main/java/com/bale_bootcamp/guardiannews/viewmodel/NewsFragmentViewModel.kt
@@ -1,16 +1,17 @@
 package com.bale_bootcamp.guardiannews.viewmodel
 
-import android.util.Log
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
+
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.switchMap
-import com.bale_bootcamp.guardiannews.model.ResponseModel
+import androidx.lifecycle.viewModelScope
+import com.bale_bootcamp.guardiannews.GuardianNewsApp
+import com.bale_bootcamp.guardiannews.model.News
 import com.bale_bootcamp.guardiannews.network.NewsApi
 import com.bale_bootcamp.guardiannews.network.NewsApiService
 import com.bale_bootcamp.guardiannews.onlinedatasources.NewsOnlineDataSource
 import com.bale_bootcamp.guardiannews.repository.NewsRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.launch
 import java.time.LocalDate
 
 class NewsFragmentViewModel (
@@ -22,15 +23,29 @@ class NewsFragmentViewModel (
                 fromDate: LocalDate,
                 toDate: LocalDate,
                 page: Int,
-                pageSize: Int): LiveData<ResponseModel?> {
-        return repository.getNews(category, fromDate, toDate, page, pageSize)
+                pageSize: Int
+    ): Flow<List<News>> = repository.getNews(category, fromDate, toDate, page, pageSize)
+
+
+    fun refreshNews(category: NewsApiService.Category,
+                    fromDate: LocalDate,
+                    toDate: LocalDate,
+                    page: Int,
+                    pageSize: Int) {
+        viewModelScope.launch{
+            repository.refreshNews(category, fromDate, toDate, page, pageSize)
+        }
     }
 
 
-    class NewsFragmentViewModelFactory(): ViewModelProvider.Factory {
+    class NewsFragmentViewModelFactory: ViewModelProvider.Factory {
         override fun <T : ViewModel> create(modelClass: Class<T>): T {
             if (modelClass.isAssignableFrom(NewsFragmentViewModel::class.java)) {
-                val repository = NewsRepository((NewsOnlineDataSource(NewsApi.retrofitApiService)))
+                val repository
+                    = NewsRepository(
+                        NewsOnlineDataSource(NewsApi.retrofitApiService),
+                        GuardianNewsApp.getAppContext().database.newsDao()
+                    )
                 return NewsFragmentViewModel(repository) as T
             }
             throw IllegalArgumentException("Unknown ViewModel class")
