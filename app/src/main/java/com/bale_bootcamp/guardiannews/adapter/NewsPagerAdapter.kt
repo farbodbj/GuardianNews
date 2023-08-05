23b21dd77468427967c5c746a5c78897b46a22f7
Farbod Bijary <110523279+farbodbj@users.noreply.github.com>
2023-08-05 12:57:17 +0330

    removed fragment constructor args

--- app/src/main/java/com/bale_bootcamp/guardiannews/adapter/NewsPagerAdapter.kt
+++ app/src/main/java/com/bale_bootcamp/guardiannews/adapter/NewsPagerAdapter.kt
@@ -1,6 +1,7 @@
 package com.bale_bootcamp.guardiannews.adapter
 
 
+import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
@@ -20,7 +21,11 @@ class NewsPagerAdapter(fragmentActivity: FragmentActivity,
 
     override fun createFragment(position: Int): Fragment {
         Log.d(TAG, "createFragment: category at position: ${categories[position]}")
-        return NewsFragment(categories[position])
+        val newsFragment = NewsFragment()
+        newsFragment.arguments = Bundle().apply {
+            putString("category", categories[position])
+        }
+        return newsFragment
     }
 
 }
