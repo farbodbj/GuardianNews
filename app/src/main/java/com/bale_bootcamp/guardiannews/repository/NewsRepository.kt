211741364b0722f531b03a0041281d63e1c6d650
Farbod Bijary <110523279+farbodbj@users.noreply.github.com>
2023-08-05 12:16:35 +0330

    NewsRepository adapted to paging

--- app/src/main/java/com/bale_bootcamp/guardiannews/repository/NewsRepository.kt
+++ app/src/main/java/com/bale_bootcamp/guardiannews/repository/NewsRepository.kt
@@ -2,9 +2,16 @@ package com.bale_bootcamp.guardiannews.repository
 
 
 import android.util.Log
+import androidx.paging.ExperimentalPagingApi
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import com.bale_bootcamp.guardiannews.GuardianNewsApp
 import com.bale_bootcamp.guardiannews.localdatasources.database.NewsDao
 import com.bale_bootcamp.guardiannews.model.News
 import com.bale_bootcamp.guardiannews.network.NewsApiService
+import com.bale_bootcamp.guardiannews.pagination.NewsPagingMediator
+import com.bale_bootcamp.guardiannews.pagination.RemoteKeyDao
 import kotlinx.coroutines.flow.Flow
 
 import java.time.LocalDate
@@ -28,11 +35,32 @@ class NewsRepository (
         }
     }
 
+    @OptIn(ExperimentalPagingApi::class)
     fun getNews(category: NewsApiService.Category,
                 fromDate: LocalDate,
                 toDate: LocalDate,
                 page: Int,
                 pageSize: Int
-    ): Flow<List<News>> =
-        localDataSource.select(category)
+    ): Flow<PagingData<News>> {
+        val pagingConfig = PagingConfig(
+            pageSize = 10,
+            prefetchDistance = 8,
+            enablePlaceholders = false
+        )
+
+        val pager = Pager(
+            config = pagingConfig,
+            remoteMediator = NewsPagingMediator(onlineDataSource,
+                localDataSource,
+                GuardianNewsApp.getAppContext().database.remoteKeyDao(),
+                category,
+                fromDate,
+                toDate)) {
+            Log.d(TAG, "localDataSource.selectAll()")
+            localDataSource.select(category)
+        }
+
+        // add caching if feasible
+        return pager.flow
+    }
 }
