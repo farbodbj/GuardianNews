f29e072a3055d9cf09b5b602b437edef41d06e3a
Farbod Bijary <110523279+farbodbj@users.noreply.github.com>
2023-08-07 20:52:02 +0330

    theme implementation cleaned up

--- app/src/main/java/com/bale_bootcamp/guardiannews/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/bale_bootcamp/guardiannews/ui/settings/SettingsFragment.kt
@@ -7,16 +7,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.LinearLayout
-import androidx.datastore.dataStore
-import androidx.datastore.preferences.preferencesDataStore
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import com.bale_bootcamp.guardiannews.R
-import com.bale_bootcamp.guardiannews.data.local.datastore.SettingsDataStore
 import com.bale_bootcamp.guardiannews.databinding.AlertDialogThemeChoiceBinding
 import com.bale_bootcamp.guardiannews.databinding.FragmentSettingsBinding
-import com.bale_bootcamp.guardiannews.ui.MainActivity
 import com.bale_bootcamp.guardiannews.ui.settings.model.ColorTheme
 import kotlinx.coroutines.async
 import kotlinx.coroutines.awaitAll
@@ -56,79 +51,91 @@ class SettingsFragment : Fragment() {
         setSettingsOnClickListeners()
     }
 
-    private fun setSettingsOnClickListeners() {
-        Log.d(TAG, "setSettingsOnClickListeners: started")
-        binding.themeSetting.root.setOnClickListener {
-            Log.d(TAG, "themeSetting clicked")
-            val themeChangeAlertDialogBuilder = AlertDialog.Builder(requireContext()).create()
-            val themeChangeAlertDialogView = layoutInflater.inflate(R.layout.alert_dialog_theme_choice, null)
-            val alertDialogViewBinding = AlertDialogThemeChoiceBinding.bind(themeChangeAlertDialogView)
-            themeChangeAlertDialogBuilder.setView(themeChangeAlertDialogView)
-            themeChangeAlertDialogBuilder.show()
-            alertDialogViewBinding.apply {
-                okButton.setOnClickListener {
-                    themeSelectionToggleGroup.checkedButtonId.let {
-                        Log.d(TAG, "checked button id: $it")
-                        val selectedTheme = when(it) {
-                            R.id.white_button -> ColorTheme.WHITE
-                            R.id.sky_blue_button -> ColorTheme.SKY_BLUE
-                            R.id.dark_blue_button -> ColorTheme.DARK_BLUE
-                            R.id.green_button -> ColorTheme.GREEN
-                            R.id.light_green_button -> ColorTheme.LIGHT_GREEN
-                            else -> throw IllegalStateException("unknown button chosen")
-                        }
-                        Log.d(TAG, "checked button related theme color: ${selectedTheme.value}")
-                        viewModel.saveColorTheme(selectedTheme)
-                    }
-                    Log.d(TAG, "recreating the activity")
-                    activity?.recreate()
-                    themeChangeAlertDialogBuilder.dismiss()
-                }
-            }
-        }
-    }
-
     private fun startObservingSettings() {
         lifecycleScope.launch {
             Log.d(TAG, "launching observation on all theme items")
             awaitAll(observerItemCountAsync(),
-                    observerOrderByAsync(),
-                    observerFromDateAsync(),
-                    observerColorThemeAsync(),
-                    observerTextSizeAsync())
+                observerOrderByAsync(),
+                observerFromDateAsync(),
+                observerColorThemeAsync(),
+                observerTextSizeAsync())
         }
     }
 
     private suspend fun observerItemCountAsync() = lifecycleScope.async {
         viewModel.itemCount.collect {
-        binding.itemCountSetting.settingValue.text = it.toString()
+            binding.itemCountSetting.settingValue.text = it.toString()
         }
     }
 
     private suspend fun observerOrderByAsync() = lifecycleScope.async {
         viewModel.orderBy.collect {
-        binding.orderBySetting.settingValue.text = it.value
+            binding.orderBySetting.settingValue.text = it.value
         }
     }
 
     private suspend fun observerFromDateAsync() = lifecycleScope.async {
         viewModel.fromDate.collect {
-        binding.fromDateSetting.settingValue.text = it
+            binding.fromDateSetting.settingValue.text = it
         }
     }
 
     private suspend fun observerColorThemeAsync() = lifecycleScope.async {
         viewModel.colorTheme.collect {
-        binding.themeSetting.settingValue.text = it.value
+            binding.themeSetting.settingValue.text = it.value
         }
     }
 
     private suspend fun observerTextSizeAsync() = lifecycleScope.async {
         viewModel.textSize.collect {
-        binding.textSizeSetting.settingValue.text = it.value
+            binding.textSizeSetting.settingValue.text = it.value
         }
     }
 
+
+    private fun setSettingsOnClickListeners() = binding.themeSetting.root
+        .setOnClickListener {
+            val (alertDialogViewBinding, themeChangeAlertDialog) = showThemeAlertDialog()
+            handleThemeSelection(alertDialogViewBinding, themeChangeAlertDialog)
+        }
+
+
+    private fun showThemeAlertDialog(): Pair<AlertDialogThemeChoiceBinding, AlertDialog> {
+        val themeChangeAlertDialog = AlertDialog.Builder(requireContext()).create()
+        val themeChangeAlertDialogView = layoutInflater.inflate(R.layout.alert_dialog_theme_choice, null)
+        val alertDialogViewBinding = AlertDialogThemeChoiceBinding.bind(themeChangeAlertDialogView)
+        themeChangeAlertDialog.setView(themeChangeAlertDialogView)
+        themeChangeAlertDialog.setCanceledOnTouchOutside(true)
+        themeChangeAlertDialog.show()
+        return Pair(alertDialogViewBinding, themeChangeAlertDialog)
+    }
+
+    private fun colorTheme(resId: Int): ColorTheme = when(resId) {
+            R.id.white_button -> ColorTheme.WHITE
+            R.id.sky_blue_button -> ColorTheme.SKY_BLUE
+            R.id.dark_blue_button -> ColorTheme.DARK_BLUE
+            R.id.green_button -> ColorTheme.GREEN
+            R.id.light_green_button -> ColorTheme.LIGHT_GREEN
+            else -> throw IllegalStateException("unknown button chosen")
+    }
+
+    private fun handleThemeSelection(alertDialogViewBinding: AlertDialogThemeChoiceBinding, themeChangeAlertDialog: AlertDialog) {
+        Log.d(TAG, "handleThemeSelection: started")
+
+        alertDialogViewBinding.apply {
+            okButton.setOnClickListener {
+                themeSelectionToggleGroup.checkedButtonId
+                    .let {
+                        Log.d(TAG, "checked button id: $it")
+                        val selectedTheme = colorTheme(it)
+                        Log.d(TAG, "checked button related theme color: ${selectedTheme.value}")
+                        viewModel.saveColorTheme(selectedTheme)
+                    }
+                Log.d(TAG, "recreating the activity")
+                themeChangeAlertDialog.dismiss()
+            }
+        }
+    }
 
     override fun onDestroyView() {
         super.onDestroyView()
