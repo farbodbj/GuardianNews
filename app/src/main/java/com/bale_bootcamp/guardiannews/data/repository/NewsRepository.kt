3285aa137dad02566bcc0acbf7801170070f6bef
Farbod Bijary <110523279+farbodbj@users.noreply.github.com>
2023-08-08 12:44:28 +0330

    read api call params from datastore

--- app/src/main/java/com/bale_bootcamp/guardiannews/data/repository/NewsRepository.kt
+++ app/src/main/java/com/bale_bootcamp/guardiannews/data/repository/NewsRepository.kt
@@ -6,27 +6,57 @@ import androidx.paging.ExperimentalPagingApi
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import androidx.paging.PagingData
+import androidx.paging.cachedIn
 import com.bale_bootcamp.guardiannews.GuardianNewsApp
 import com.bale_bootcamp.guardiannews.data.local.database.NewsDao
 import com.bale_bootcamp.guardiannews.data.local.model.News
 import com.bale_bootcamp.guardiannews.data.network.NewsApiService
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.launch
 
 import java.time.LocalDate
 
 class NewsRepository (
     private val onlineDataSource: NewsApiService,
-    private val localDataSource: NewsDao
+    private val localDataSource: NewsDao,
+    private val settingsRepository: SettingsRepository
 ) {
     private val TAG: String = "NewsRepository"
 
-    suspend fun refreshNews(category: NewsApiService.Category,
-                            fromDate: LocalDate,
-                            toDate: LocalDate,
-                            page: Int,
-                            pageSize: Int) {
+    private var pageSize: Int = 10
+    private var fromDate: LocalDate = LocalDate.now().minusMonths(1)
 
-        val results = onlineDataSource.getLatestFromCategory(category, fromDate, toDate, page, pageSize).response
+    private val pagingConfig: PagingConfig by lazy {
+        setPageSize()
+        PagingConfig(
+            pageSize = pageSize,
+            prefetchDistance = pageSize - 2,
+            enablePlaceholders = false
+        )
+    }
+
+    private fun setPageSize() {
+        CoroutineScope(Dispatchers.IO).launch {
+            settingsRepository.getItemCount().collect {
+                Log.d(TAG, "getPageSize collected from preferences datastore: $it")
+                pageSize = it
+            }
+        }
+    }
+
+    suspend fun refreshNews(category: NewsApiService.Category,
+                            toDate: LocalDate,
+                            page: Int) {
+
+        val results = onlineDataSource.getLatestFromCategory(category,
+            fromDate,
+            toDate,
+            page,
+            pageSize
+        ).response
+
         results.results.let {
             Log.d(TAG, it.toString())
             localDataSource.insertAll(*it.toTypedArray())
@@ -34,17 +64,7 @@ class NewsRepository (
     }
 
     @OptIn(ExperimentalPagingApi::class)
-    fun getNews(category: NewsApiService.Category,
-                fromDate: LocalDate,
-                toDate: LocalDate,
-                page: Int,
-                pageSize: Int
-    ): Flow<PagingData<News>> {
-        val pagingConfig = PagingConfig(
-            pageSize = 10,
-            prefetchDistance = 8,
-            enablePlaceholders = false
-        )
+    fun getNews(category: NewsApiService.Category, toDate: LocalDate): Flow<PagingData<News>> {
 
         val pager = Pager(
             config = pagingConfig,
@@ -55,7 +75,7 @@ class NewsRepository (
                 fromDate,
                 toDate)
         ) {
-            Log.d(TAG, "localDataSource.selectAll()")
+            Log.d(TAG, "getting news with page config: $pagingConfig for category: $category, fromDate: $fromDate, toDate: $toDate")
             localDataSource.select(category)
         }
 
