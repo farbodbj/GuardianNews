9211edc5d9cddef766b2e010ad34c8fa8aa25cf0
Farbod Bijary <110523279+farbodbj@users.noreply.github.com>
2023-08-09 17:35:37 +0330

    set font size from preferences datastore

--- app/src/main/java/com/bale_bootcamp/guardiannews/ui/MainActivity.kt
+++ app/src/main/java/com/bale_bootcamp/guardiannews/ui/MainActivity.kt
@@ -1,18 +1,16 @@
 package com.bale_bootcamp.guardiannews.ui
 
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.lifecycleScope
 import com.bale_bootcamp.guardiannews.R
 import com.bale_bootcamp.guardiannews.data.local.datastore.SettingsDataStore
 import com.bale_bootcamp.guardiannews.data.repository.SettingsRepository
 import com.bale_bootcamp.guardiannews.databinding.ActivityMainBinding
+import com.bale_bootcamp.guardiannews.ui.settings.model.TextSize
 import com.bale_bootcamp.guardiannews.utility.Utils.getColorThemeOverlayId
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
 class MainActivity : AppCompatActivity() {
@@ -29,6 +27,7 @@ class MainActivity : AppCompatActivity() {
         setThemeFromPrefs()
         Log.d(TAG, "onCreate: starts")
         super.onCreate(savedInstanceState)
+        setFontScaleFromPrefs(this)
         binding = ActivityMainBinding.inflate(layoutInflater)
         Log.d(TAG, "onCreate: binding created")
         setContentView(binding.root)
@@ -41,7 +40,32 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    override fun attachBaseContext(newBase: Context?) {
+        Log.d(TAG, "attachBaseContext called, is newBase null: ${newBase == null}")
+        if(baseContext == null) super.attachBaseContext(newBase)
+        else super.attachBaseContext(setFontScaleFromPrefs(newBase!!))
+    }
+
+    private fun setFontScaleFromPrefs(context: Context): Context {
+        val config = context.resources.configuration
+        config.fontScale = getFontSizeFromPrefs()
+        Log.d(TAG, "setting scaling all fonts to ${config.fontScale}")
+        return context.createConfigurationContext(config)
+    }
+
+    private fun getFontSizeFromPrefs() = runBlocking {
+        Log.d(TAG, "getFontSizeFromPrefs called")
+        val it = settingsRepository.getFontSize().first()
+        when(TextSize.findByStr(it)) {
+            TextSize.SMALL -> 0.85F
+            TextSize.MEDIUM -> 1.0F
+            TextSize.LARGE -> 1.15F
+        }
+    }
+
+
     private fun setThemeFromPrefs() = runBlocking {
+        Log.d(TAG, "setThemeFromPrefs called")
         val it = settingsRepository.getColorTheme().first()
             runOnUiThread {
                 val themeId = getColorThemeOverlayId(it)
