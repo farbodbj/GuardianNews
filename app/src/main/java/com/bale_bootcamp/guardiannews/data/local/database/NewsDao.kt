813b7f7ce5614117c53017be0f5a429acd2bb89f
Farbod Bijary <110523279+farbodbj@users.noreply.github.com>
2023-08-10 11:29:01 +0330

    news order selection implemented

--- app/src/main/java/com/bale_bootcamp/guardiannews/data/local/database/NewsDao.kt
+++ app/src/main/java/com/bale_bootcamp/guardiannews/data/local/database/NewsDao.kt
@@ -6,20 +6,32 @@ import androidx.room.Query
 import com.bale_bootcamp.guardiannews.data.local.BaseDao
 import com.bale_bootcamp.guardiannews.data.local.model.News
 import com.bale_bootcamp.guardiannews.data.network.NewsApiService
+import com.bale_bootcamp.guardiannews.ui.settings.model.OrderBy
 
 @Dao
 interface NewsDao: BaseDao<News> {
-    @Query("select * from ${News.ENTITY_NAME} where sectionId = :category")
-    fun selectInternal(category: String): PagingSource<Int, News>
-
     @Query("select * from ${News.ENTITY_NAME}")
-    fun selectAll(): PagingSource<Int, News>
+    fun selectAllDefault(): PagingSource<Int, News>
 
-    fun select(category: NewsApiService.Category): PagingSource<Int, News> {
+    @Query("select * from ${News.ENTITY_NAME} where sectionId = :category")
+    fun selectByCategory(category: String): PagingSource<Int, News>
+
+    @Query("select * from ${News.ENTITY_NAME} order by webPublicationDate desc")
+    fun selectAllDesc(orderBy: String): PagingSource<Int, News>
+
+    @Query("select * from ${News.ENTITY_NAME} order by webPublicationDate asc")
+    fun selectAllAsc(orderBy: String): PagingSource<Int, News>
+
+    fun select(category: NewsApiService.Category, orderBy: OrderBy): PagingSource<Int, News> {
         return if (category == NewsApiService.Category.HOME) {
-            selectAll()
+            val orderByStr = orderBy.toString()
+            when(orderBy) {
+                OrderBy.NEWEST -> selectAllDesc(orderByStr)
+                OrderBy.OLDEST -> selectAllAsc(orderByStr)
+                OrderBy.RELEVANCE -> selectAllDefault()
+            }
         } else {
-            selectInternal(category.categoryName)
+            selectByCategory(category.categoryName)
         }
     }
 
