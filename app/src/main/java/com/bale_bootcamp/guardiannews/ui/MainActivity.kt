761405393fa6ba11a7ef45a13ea3a85d3b2d3762
Farbod Bijary <110523279+farbodbj@users.noreply.github.com>
2023-08-15 12:16:19 +0330

    implemented "press back again to exit"

--- app/src/main/java/com/bale_bootcamp/guardiannews/ui/MainActivity.kt
+++ app/src/main/java/com/bale_bootcamp/guardiannews/ui/MainActivity.kt
@@ -3,9 +3,15 @@ package com.bale_bootcamp.guardiannews.ui
 import android.content.Context
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModel
+import androidx.navigation.NavHost
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import com.bale_bootcamp.guardiannews.GuardianNewsApp
 import com.bale_bootcamp.guardiannews.R
 import com.bale_bootcamp.guardiannews.data.repository.SettingsRepository
@@ -32,7 +38,8 @@ private const val TAG = "MainActivity"
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    @ApplicationContext lateinit var appContext: Context
+
+    private var backPressedTime: Long = 0
 
     @InstallIn(SingletonComponent::class)
     @EntryPoint
@@ -45,9 +52,37 @@ class MainActivity : AppCompatActivity() {
         Log.d(TAG, "onCreate: starts")
         super.onCreate(savedInstanceState)
         setFontScaleFromPrefs(this)
+        addBackButtonCallback()
         binding = ActivityMainBinding.inflate(layoutInflater)
         Log.d(TAG, "onCreate: binding created")
         setContentView(binding.root)
+    }
+
+    private fun addBackButtonCallback() {
+        val navController = Navigation.findNavController(this, R.id.fragment_container)
+        val callback = object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                val currentId = navController.currentDestination?.id
+
+                if(currentId == R.id.defaultFragment) {
+                    if (System.currentTimeMillis() - backPressedTime < 2000) {
+                        isEnabled = false
+                        finish()
+                    } else {
+                        Toast.makeText(
+                            applicationContext,
+                            getString(R.string.press_back_to_exit),
+                            Toast.LENGTH_SHORT)
+                        .show()
+                    }
+                    backPressedTime = System.currentTimeMillis()
+                } else {
+                    navController.navigateUp()
+                }
+            }
+        }
+
+        onBackPressedDispatcher.addCallback(this, callback)
     }
 
     override fun attachBaseContext(newBase: Context?) {
