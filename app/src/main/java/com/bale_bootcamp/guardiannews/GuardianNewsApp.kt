cf0daf7712286ef54991639a995056e9e4759797
Farbod Bijary <110523279+farbodbj@users.noreply.github.com>
2023-08-16 17:14:58 +0330

    unnesessary properties deleted from application class

--- app/src/main/java/com/bale_bootcamp/guardiannews/GuardianNewsApp.kt
+++ app/src/main/java/com/bale_bootcamp/guardiannews/GuardianNewsApp.kt
@@ -1,25 +1,9 @@
 package com.bale_bootcamp.guardiannews
 
 import android.app.Application
-import android.util.Log
-import androidx.room.RoomDatabase
-import com.bale_bootcamp.guardiannews.data.local.database.AppDatabase
 import dagger.hilt.android.HiltAndroidApp
-import java.util.concurrent.Executors.newSingleThreadExecutor
 
 @HiltAndroidApp
 class GuardianNewsApp: Application() {
     val name = "GuardianNewsApp"
-
-    override fun onCreate() {
-        super.onCreate()
-        appContext = this
-    }
-
-    companion object {
-        private lateinit var appContext: GuardianNewsApp
-        fun getAppContext(): GuardianNewsApp {
-            return appContext
-        }
-    }
 }
