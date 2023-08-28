743426f47e500853841e4c3d49d5a468999f666c
Farbod Bijary <110523279+farbodbj@users.noreply.github.com>
2023-08-28 15:07:11 +0330

    date format changed

--- app/src/main/java/com/bale_bootcamp/guardiannews/utility/LocalDateTimeAdapter.kt
+++ app/src/main/java/com/bale_bootcamp/guardiannews/utility/LocalDateTimeAdapter.kt
@@ -7,16 +7,20 @@ import com.squareup.moshi.JsonWriter
 import com.squareup.moshi.ToJson
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
+import java.util.Date
 
 class LocalDateTimeAdapter: JsonAdapter<LocalDateTime>() {
-    private val formatter = DateTimeFormatter.ISO_DATE_TIME
+    companion object {
+        val isoFormatter = DateTimeFormatter.ISO_DATE_TIME
+        val casualFormatter = DateTimeFormatter.ofPattern("MMM d, yyyy");
+    }
     @FromJson
     override fun fromJson(reader: JsonReader): LocalDateTime? {
-        return LocalDateTime.parse(reader.nextString(), formatter)
+        return LocalDateTime.parse(reader.nextString(), isoFormatter)
     }
 
     @ToJson
     override fun toJson(writer: JsonWriter, value: LocalDateTime?) {
-        writer.value(value?.format(formatter))
+        writer.value(value?.format(isoFormatter))
     }
 }
